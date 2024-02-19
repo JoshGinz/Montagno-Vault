821d4672b3ef2b878dcad464d869228485ed080e
joshginz <86862783+JoshGinz@users.noreply.github.com>
2024-02-18 20:02:57 -0500

    s

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,6 +17,33 @@ const firebaseConfig = {
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
   
+
+  // Get the modal
+var modal = document.getElementById('uploadModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById('uploadBtn');
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName('close')[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
 document.getElementById('uploadForm').addEventListener('submit', function(e) {
 		e.preventDefault();
 	  
